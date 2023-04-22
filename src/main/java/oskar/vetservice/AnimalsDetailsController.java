e841b99c75e91178548012577ce7df585a6fb015
Oskar <oskszysiak@gmail.com>
2023-04-22 17:15:39 +0200

    Added animals details view and its controller

---
+++ src/main/java/oskar/vetservice/AnimalsDetailsController.java
@@ -0,0 +1,27 @@
+package oskar.vetservice;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class AnimalsDetailsController {
+    @FXML
+    private ImageView animalPhoto;
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label speciesLabel;
+    @FXML
+    private Label birthdayAndAgeLabel;
+    @FXML
+    private Label genderLabel;
+    @FXML
+    private Label nameAndSurnameLabel;
+    @FXML
+    private Label emailLabel;
+    @FXML
+    private Label phoneNumberLabel;
+    @FXML
+    private Label addressLabel;
+
+}
