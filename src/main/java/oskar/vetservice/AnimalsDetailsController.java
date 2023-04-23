768e4eb840e50c1e80bfa067c6e35adf6b9d28a5
Oskar <oskszysiak@gmail.com>
2023-04-23 22:40:24 +0200

    Added functions to set content of new animal details window

--- src/main/java/oskar/vetservice/AnimalsDetailsController.java
+++ src/main/java/oskar/vetservice/AnimalsDetailsController.java
@@ -2,26 +2,91 @@ package oskar.vetservice;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class AnimalsDetailsController {
     @FXML
     private ImageView animalPhoto;
     @FXML
-    private Label nameLabel;
+    private Label animalNameLabel;
     @FXML
-    private Label speciesLabel;
+    private Label animalSpeciesLabel;
     @FXML
-    private Label birthdayAndAgeLabel;
+    private Label animalBirthdayAndAgeLabel;
     @FXML
-    private Label genderLabel;
+    private Label animalGenderLabel;
     @FXML
-    private Label nameAndSurnameLabel;
+    private Label ownerNameAndSurnameLabel;
     @FXML
-    private Label emailLabel;
+    private Label ownerEmailLabel;
     @FXML
-    private Label phoneNumberLabel;
+    private Label ownerPhoneNumberLabel;
     @FXML
-    private Label addressLabel;
+    private Label ownerAddressLabel;
 
+    public void setImage(String path){
+        if(path.compareTo("none") == 0){
+            return;
+        }
+
+        animalPhoto.setImage(new Image(path));
+    }
+
+    public void setAnimalNameLabel(String name){
+        animalNameLabel.setText(name);
+    }
+
+    public void setAnimalSpeciesLabel(String species) {
+        animalSpeciesLabel.setText(species);
+    }
+
+    public void setAnimalBirthdayAndAgeLabel(LocalDate birthday) {
+        int ageInMonths = (int) ChronoUnit.MONTHS.between(birthday, LocalDate.now());
+
+        int ageInYears = ageInMonths / 12;
+        int monthsLeft = ageInMonths - ageInYears * 12;
+
+        animalBirthdayAndAgeLabel.setText(birthday.toString() + " (" + ageInYears + " years "
+                + monthsLeft + " months)" );
+    }
+
+    public void setAnimalGenderLabel(String gender) {
+        animalGenderLabel.setText(gender);
+    }
+
+    public void setOwnerNameAndSurnameLabel(String name, String surname) {
+        ownerNameAndSurnameLabel.setText(name + " " + surname);
+    }
+
+    public void setOwnerEmailLabel(String email) {
+        ownerEmailLabel.setText(email);
+    }
+
+    public void setOwnerPhoneNumberLabel(String phoneNumber) {
+        ownerPhoneNumberLabel.setText(phoneNumber);
+    }
+
+    public void setOwnerAddressLabel(String city, String street, String houseNumber) {
+        ownerAddressLabel.setText(city + " " + street + " " + houseNumber);
+    }
+
+
+    public void setContent(String imagePath, String animalName, String animalSpecies, LocalDate animalBirthday, String animalGender,
+                           String ownerName, String ownerSurname, String ownerEmail, String ownerPhoneNumber, String ownerCity,
+                           String ownerStreet, String ownerHouseNumber) {
+
+        setImage(imagePath);
+        setAnimalNameLabel(animalName);
+        setAnimalSpeciesLabel(animalSpecies);
+        setAnimalBirthdayAndAgeLabel(animalBirthday);
+        setAnimalGenderLabel(animalGender);
+        setOwnerNameAndSurnameLabel(ownerName, ownerSurname);
+        setOwnerEmailLabel(ownerEmail);
+        setOwnerPhoneNumberLabel(ownerPhoneNumber);
+        setOwnerAddressLabel(ownerCity, ownerStreet, ownerHouseNumber);
+    }
 }
